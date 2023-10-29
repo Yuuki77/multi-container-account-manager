6058aff5f59a995887cda3a0ae2168bb6318410e
Yuuki77 <yuki.osaki7@gmail.com>
2023-10-30 02:16:06 +0900

    init

---
+++ README.md
@@ -0,0 +1,64 @@
+# Multi-Container Account Manager for Firefox
+
+## Description
+
+This Firefox extension allows you to manage multiple container tabs. You can delete existing containers, extract container information to a CSV file, and create new containers from a CSV file.
+
+## Features
+
+- **Delete Existing Containers**: Remove all existing containers.
+- **Extract Existing Containers**: Export information of existing containers to a CSV file.
+- **Create Containers from CSV**: Upload a CSV file to create new containers with optional URLs, emails, and passwords.
+
+## Prerequisites
+
+- Firefox Browser
+- Firefox Multi-Container Account https://addons.mozilla.org/en-US/firefox/addon/multi-account-containers/
+
+## Installation
+
+1. Clone this repository or download the ZIP file.
+2. Open Firefox and go to `about:debugging#/runtime/this-firefox`.
+3. Click on "Load Temporary Add-on" and select the `manifest.json` from this project's directory.
+
+## Usage
+
+### Deleting Existing Containers
+
+1. Click on the extension icon.
+2. Click the "Delete Existing Containers" button.
+
+### Extracting Existing Containers
+
+1. Click on the extension icon.
+2. Click the "Extract Existing Containers" button.
+3. A CSV file named `existing_containers.csv` will be downloaded.
+
+### Creating Containers from a Textarea Input (CSV formatted)
+
+1. Click on the extension icon.
+2. Paste your CSV content into the textarea.
+3. Click the "Upload" button.
+
+## CSV Format
+
+The CSV should have the following columns:
+
+- `name`: Container name
+- `url`: (Optional) URL to open in the container
+- `email`: (Optional) Email to autofill
+- `password`: (Optional) Password to autofill
+- `color`: (Optional) Container color
+- `icon`: (Optional) Container icon
+
+Example:
+```
+name,url,email,password,color,icon
+GithubA,https://github.com/login,email@gmail.com,password123,red,fingerprint
+GithubB,https://github.com/login,email2@gmail.com,password123,blue,fingerprint
+```
+
+## Troubleshooting
+
+If you encounter any issues related to permissions, ensure that you have correctly set the permissions in the `manifest.json` file.
+
---
+++ popup.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", function () {
+  // Delete existing containers
+  document.getElementById("delete").addEventListener("click", function () {
+    browser.contextualIdentities.query({}).then((identities) => {
+      if (identities.length === 0) {
+        console.log("No containers found.");
+        alert("No containers found.");
+        return;
+      }
+      identities.forEach((identity) => {
+        browser.contextualIdentities.remove(identity.cookieStoreId);
+      });
+      alert("All containers have been deleted.");
+    });
+  });
+
+  // Extract existing container information
+  document.getElementById("extract").addEventListener("click", function () {
+    browser.contextualIdentities.query({}).then((identities) => {
+      const csvContent = ["name,url,email,password,color,icon"];
+
+      identities.forEach((identity) => {
+        const { name, color, icon } = identity;
+        csvContent.push([name, "null", "null", "null", color, icon].join(","));
+      });
+
+      const csvString = csvContent.join("\n");
+      const blob = new Blob([csvString], { type: "text/csv;charset=utf-8;" });
+      const link = document.createElement("a");
+      const url = URL.createObjectURL(blob);
+      link.setAttribute("href", url);
+      link.setAttribute("download", "existing_containers.csv");
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    });
+  });
+
+  // Process CSV from textarea
+  document
+    .getElementById("processCSV")
+    .addEventListener("click", async function () {
+      const csvContent = document.getElementById("csvInput").value;
+      const lines = csvContent.split("\n").slice(1);
+      for (const line of lines) {
+        const [name, url, email, password, color, icon] = line.split(",");
+        if (name) {
+          try {
+            const identity = await browser.contextualIdentities.create({
+              name,
+              color: color || "blue",
+              icon: icon || "fingerprint",
+            });
+
+            // Open the URL in the newly created container tab
+            if (url) {
+              const tab = await browser.tabs.create({
+                url,
+                cookieStoreId: identity.cookieStoreId,
+              });
+
+              // Inject the content script to fill email and password
+              if (email && password) {
+                const code = `
+                const inputs = document.querySelectorAll('input');
+                inputs.forEach((input) => {
+                  if (input.getAttribute('autocomplete') === 'username' || input.id === 'email' || input.name === 'login' || input.id === 'login_field') {
+                    input.value = "${email}";
+                  }
+                  if (input.getAttribute('autocomplete') === 'current-password' || input.name === 'pass' || input.name === 'password' || input.id === 'password') {
+                    input.value = "${password}";
+                  }
+                });
+              `;
+                await browser.tabs.executeScript(tab.id, { code });
+              }
+            }
+          } catch (e) {
+            alert(e);
+          }
+        }
+      }
+    });
+});
